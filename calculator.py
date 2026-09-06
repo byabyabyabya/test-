9ec2d4403b8f475dfb1fade3d8babcbc2479d206
unknown <sasha.zubach.0l0@gmail.com>
2026-09-06 20:07:28 +0300

    бананы

---
+++ calculator.py
@@ -0,0 +1,23 @@
+# operation = input("введите операцию: (сложение, вычитание, умножение, деление): ").upper
+
+# if (operation == "сложение"):
+#     a = int(input("введите первое число:"))
+#     b = int(input("введите второе число:"))
+#     print(a+b)
+# elif (operation == "вычитание"):
+#     a = int(input("введите уменьшаемое:"))
+#     b = int(input("введите вычитаемое:"))
+#     print(a-b)
+# elif (operation == "умножение"):
+#     a = int(input("введите первое число:"))
+#     b = int(input("введите второе число:"))
+#     print(a*b)
+# elif (operation == "деление"):
+#     a = int(input("введите делимое:"))
+#     b = int(input("введите делитель:"))
+#     print(a/b)
+fruits = [ "яблоко", "банан", "апельсин"]
+x = 1
+for x in range(3):
+    print(fruits[x])
+    x = x+1
