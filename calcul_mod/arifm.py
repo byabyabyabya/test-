123c881a04df4976fc21e39bc1aebb2edf0dbc20
unknown <sasha.zubach.0l0@gmail.com>
2026-09-13 19:06:10 +0300

    защита от дурака

--- calcul_mod/arifm.py
+++ calcul_mod/arifm.py
@@ -5,7 +5,5 @@ def minus(a, b):
 def umnozit(a, b):
     return a*b
 def delit(a, b):
-    if ( b != 0):
         return a/b
-    else:
-        print("ошибка")
+    
--- main.py
+++ main.py
@@ -1,33 +1,40 @@
 from calcul_mod.arifm import add, minus, delit, umnozit
 from calcul_mod.advanced import power, square
-def main():
-    operation = input("введите операцию: (+, -, *, /, **, **2 ): ")
-    a = 0
-    b = 0
-    res = 0
-    if (operation == "+"):
-        a = int(input("введите первое число:"))
-        b = int(input("введите второе число:"))
-        res = add(a, b)
-    elif (operation == "-"):
-        a = int(input("введите первое число:"))
-        b = int(input("введите второе число:"))
-        res = minus(a, b)
-    elif (operation == "*"):
-        a = int(input("введите gth число:"))
-        b = int(input("введите второе число:"))
-        res = umnozit(a, b)        
-    elif (operation == "/"):
-        a = int(input("введите первое число:"))
-        b = int(input("введите второе число:"))
-        res = delit(a,b)      
-    elif (operation == "**"):
+try:
+    def main():
+        operation = input("введите операцию: (+, -, *, /, **, **2 ): ")
+        a = 0
+        b = 0
+        res = 0
+        if (operation == "+"):
             a = int(input("введите первое число:"))
             b = int(input("введите второе число:"))
-            res = power(a,b)     
-    elif (operation == "**2"):
-                a = int(input("введите число:"))
-                res = square(a)      
-    print("Результат:", res)  
-if __name__ == "__main__":
-    main()
+            res = add(a, b)
+        elif (operation == "-"):
+            a = int(input("введите первое число:"))
+            b = int(input("введите второе число:"))
+            res = minus(a, b)
+        elif (operation == "*"):
+            a = int(input("введите gth число:"))
+            b = int(input("введите второе число:"))
+            res = umnozit(a, b)        
+        elif (operation == "/"):
+            a = int(input("введите первое число:"))
+            b = int(input("введите второе число:"))
+            res = delit(a,b)      
+        elif (operation == "**"):
+                a = int(input("введите первое число:"))
+                b = int(input("введите второе число:"))
+                res = power(a,b)     
+        elif (operation == "**2"):
+                    a = int(input("введите число:"))
+                    res = square(a)    
+        else:
+             print("произошла ошибка. возможно, стоит перестать писать околесицу")  
+        print("Результат:", res)  
+    if __name__ == "__main__":
+        main()
+except ZeroDivisionError:
+     print("на ноль делить нельзя")
+except ValueError:
+     print("это не число.")
