f944d64798b20e3162d969b2c6121d87b9b8cc78
unknown <sasha.zubach.0l0@gmail.com>
2026-09-13 19:59:08 +0300

    roots

--- calcul_mod/advanced.py
+++ calcul_mod/advanced.py
@@ -2,3 +2,5 @@ def power(a, b):
     return a**b
 def square (a):
     return a*a
+def root (a):
+    return a ** 0.5
--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from calcul_mod.arifm import add, minus, delit, umnozit
-from calcul_mod.advanced import power, square
-try:
-    def main():
-        operation = input("введите операцию: (+, -, *, /, **, **2 ): ")
+from calcul_mod.advanced import power, square, root
+
+class NegativeError(Exception):
+     pass
+
+def sqrt_check(a):
+     if (a<0):
+          raise NegativeError("оштбка. корень из отрицательного числа не извлечен")
+     return root(a)
+
+def main():
+    try:
+        operation = input("введите операцию: (+, -, *, /, **, **2 , **0.5): ")
         a = 0
         b = 0
         res = 0
@@ -34,14 +43,20 @@ try:
         elif (operation == "**2"):
                     a = int(input("введите число:"))
                     res = square(a) 
-                    print("Результат:", res)     
+                    print("Результат:", res)    
+        elif (operation == "**0.5"):
+                    a = int(input("введите число:"))
+                    res = sqrt_check(a)
+                    print("Результат:", res)   
         else:
              print("произошла ошибка. возможно, стоит перестать писать околесицу")  
-    if __name__ == "__main__":
-        main()
-except ZeroDivisionError:
-     print("на ноль делить нельзя")
-except ValueError:
-     print("это не число.")
-finally:
-     print("всё.")
+    
+    except ZeroDivisionError:
+        print("на ноль делить нельзя")
+    except ValueError:
+        print("это не число.")
+    finally:
+        print("всё")
+
+if __name__ == "__main__":
+    main()
