96ce564802e5bce2c67936ea9d67c44253011f0d
unknown <sasha.zubach.0l0@gmail.com>
2026-09-13 17:24:25 +0300

    dczrj

--- calculator.py
+++ calculator.py
@@ -1,21 +1,22 @@
-# operation = input("введите операцию: (сложение, вычитание, умножение, деление): ").upper
-
-# if (operation == "сложение"):
-#     a = int(input("введите первое число:"))
-#     b = int(input("введите второе число:"))
-#     print(a+b)
-# elif (operation == "вычитание"):
-#     a = int(input("введите уменьшаемое:"))
-#     b = int(input("введите вычитаемое:"))
-#     print(a-b)
-# elif (operation == "умножение"):
-#     a = int(input("введите первое число:"))
-#     b = int(input("введите второе число:"))
-#     print(a*b)
-# elif (operation == "деление"):
-#     a = int(input("введите делимое:"))
-#     b = int(input("введите делитель:"))
-#     print(a/b)
+operation = input("введите операцию: (+, -, *, /): ")
+a = 0
+b = 0
+if (operation == "+"):
+    a = int(input("введите первое число:"))
+    b = int(input("введите второе число:"))
+    print(a+b)
+elif (operation == "-"):
+    a = int(input("введите уменьшаемое:"))
+    b = int(input("введите вычитаемое:"))
+    print(a-b)
+elif (operation == "*"):
+    a = int(input("введите первое число:"))
+    b = int(input("введите второе число:"))
+    print(a*b)
+elif (operation == "/"):
+    a = int(input("введите делимое:"))
+    b = int(input("введите делитель:"))
+    print(a/b)
 fruits = [ "яблоко", "банан", "апельсин"]
 x = 1
 for x in range(3):
