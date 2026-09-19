b5ade492308eefe0ffc8e8ace709638e0feb9795
byabyabya <ya.nikto2443@gmail.com>
2026-09-19 18:06:39 +0300

    секанс и косеканс(понятия не имею, что это)

--- calcul_mod/triganometry.py
+++ calcul_mod/triganometry.py
@@ -19,3 +19,13 @@ def cotangens(a):
     radians = math.radians(a)
     a = math.cos(radians) / math.sin(radians)
     return a
+def sec(a):
+    radians = 0
+    radians = math.radians(a)
+    a = 1 / math.cos(radians)
+    return a
+def cosec(a):
+    radians = 0
+    radians = math.radians(a)
+    a = 1 / math.sin(radians)
+    return a
